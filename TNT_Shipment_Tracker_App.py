d38e6f718b71321cfe39a81ddb4de8596bea88ea
Albert Lleida <124719215+alleida23@users.noreply.github.com>
2023-11-20 17:47:13 +0100

    Update TNT_Shipment_Tracker_App.py
    Filtered data, count in transit - exception

--- TNT_Shipment_Tracker_App.py
+++ TNT_Shipment_Tracker_App.py
@@ -8,12 +8,22 @@ def main():
 
     if uploaded_file is not None:
         df = pd.read_excel(uploaded_file)
-        st.write("Preview of the uploaded Excel file:")
-        st.write(df)
 
         if st.button("Get Last TNT Shipment Track Report"):
-            # Replace this with the actual code to generate the TNT Shipment Track Report
-            st.write("Generating TNT Shipment Track Report...")
+            # Filter data: subset where Carrier = "TNT" & Status != DELIVERED
+            shipment_to_query = df[(df["Carrier"] == "TNT") & (df["Status"] != "DELIVERED")]
+
+            # Count total shipments
+            total_shipments = len(shipment_to_query)
+
+            # Count shipments with "IN TRANSIT" and "EXCEPTION" statuses
+            shipment_in_transit = len(shipment_to_query[shipment_to_query["Status"] == "IN TRANSIT"])
+            shipment_exception = len(shipment_to_query[shipment_to_query["Status"] == "EXCEPTION"])
+
+            # Display the report
+            st.write("Total Shipments:", total_shipments)
+            st.write("IN TRANSIT:", shipment_in_transit)
+            st.write("EXCEPTION:", shipment_exception)
 
 if __name__ == "__main__":
     main()
